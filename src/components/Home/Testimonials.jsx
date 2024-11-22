b4e8914638182d297bce58a137b87b5cc900f1e2
shakil ahmed <shakiltha338@gmail.com>
2024-11-22 07:39:03 +0600

    testimonial section

--- src/components/Home/Testimonials.jsx
+++ src/components/Home/Testimonials.jsx
@@ -1,5 +1,85 @@
+import AwesomeTestimonial from "react-awesome-testimonials";
 const Testimonials = () => {
-  return <div>Testimonials</div>;
+  return (
+    <div>
+      <AwesomeTestimonial
+        testimonials={[
+          {
+            name: "Farhana rahman",
+            company: "12 Marine Drive, Chittagong, Bangladesh",
+            img_src:
+              "https://plus.unsplash.com/premium_photo-1661589856899-6dd0871f9db6?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            review:
+              "Best place to get fresh fish. Always high quality, and delivery is super quick!",
+          },
+          {
+            name: "Nasrin akther",
+            company: "45 Greenway Lane, Dhaka, Bangladesh",
+            img_src:
+              "https://plus.unsplash.com/premium_photo-1661751407469-dec56eca81a9?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            review:
+              "A wide variety of vegetables and fruits, all organic and fresh. Highly recommend!",
+          },
+          {
+            name: "Sharmin jahan",
+            company: "88 Milk Street, Rajshahi, Bangladesh",
+            img_src:
+              "https://plus.unsplash.com/premium_photo-1661683653197-ca2d0aab80eb?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDEwMnx8fGVufDB8fHx8fA%3D%3D",
+            review:
+              "Their dairy products are creamy and delicious. Perfect for everyday use!",
+          },
+          {
+            name: "Rahim hossain",
+            company: "7 Carnivore Court, Khulna, Bangladesh",
+            img_src:
+              "https://plus.unsplash.com/premium_photo-1661722270986-04efa5257deb?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDM0fHx8ZW58MHx8fHx8",
+            review:
+              "Their meat is always fresh and well-packaged. Great for barbecues!",
+          },
+          {
+            name: "Abdul karim",
+            company: "25 Chill Avenue, Sylhet, Bangladesh",
+            img_src:
+              "https://media.istockphoto.com/id/1329501064/photo/portrait-of-middle-aged-man-smiling-at-the-camera.jpg?s=2048x2048&w=is&k=20&c=L9bjViBQeabkb5OY9Em3Trih8jWIrQ2GGM0s49TYVjU=",
+            review:
+              "Amazing selection of frozen products, and everything arrives perfectly chilled.",
+          },
+          {
+            name: "Shahin alam",
+            company: "109 Orchard Road, Barisal, Bangladesh",
+            img_src:
+              "https://media.istockphoto.com/id/1296271557/photo/portrait-of-smiling-handsome-young-man-looking-at-camera.jpg?s=612x612&w=0&k=20&c=Kdio40WLhaaKjVBu2sLFVK7S66X7NJlLJiQSvGfruGo=",
+            review:
+              "Love their fruits and vegetables. The quality is unbeatable!",
+          },
+          {
+            name: "Hasan mahmud",
+            company: "42 Bazaar Street, Comilla, Bangladesh",
+            img_src:
+              "https://media.istockphoto.com/id/1266648560/photo/profile-picture-of-caucasian-male-employee-posing-in-office.jpg?s=612x612&w=0&k=20&c=jU4cNckWjhvlyjnMOpcURsG87aX0BS4N3ndUOH83M9Y=",
+            review:
+              "One-stop shop for all my grocery needs. Affordable and reliable service.",
+          },
+          {
+            name: "Sohail ahmed",
+            company: " 3 Fisherman's Wharf, Cox's Bazar, Bangladesh",
+            img_src:
+              "https://media.istockphoto.com/id/1368615000/photo/head-shot-portrait-of-smiling-confident-young-man.jpg?s=612x612&w=0&k=20&c=KdVbO-CtcE2VS1bCutXOZmgwbr1EQoyFj0kVpJGRQPA=",
+            review:
+              "The best seafood collection in the area. Always fresh and full of flavor!",
+          },
+          {
+            name: "Selim hasan",
+            company: "64 Chillberry Drive, Rangpur, Bangladesh",
+            img_src:
+              "https://media.istockphoto.com/id/1308884045/photo/portrait-of-smiling-businessman-have-webcam-call.jpg?s=612x612&w=0&k=20&c=6g6TSoSj5BxUXfNdRKC9tInELqisztmqQnN5zZP4AqE=",
+            review:
+              "A good mix of frozen and grocery products. Convenient and great service!",
+          },
+        ]}
+      />
+    </div>
+  );
 };
 
 export default Testimonials;
